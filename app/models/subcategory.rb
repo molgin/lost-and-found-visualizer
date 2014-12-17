77a3d01b6e7ffe4556fc0bf7c05d9e1d04827fc0
molgin <molgin@users.noreply.github.com>
2014-12-16 22:57:17 -0500

    experiment with finding recent versions

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,4 +1,11 @@
 class Subcategory < ActiveRecord::Base
   has_paper_trail
   belongs_to :category
+
+  def changes_in_last_day
+    # this should be in some kinda module
+    versions.where('created_at >= :one_day_ago',
+    :one_day_ago  => Time.now - 1.day)
+  end
+
 end
