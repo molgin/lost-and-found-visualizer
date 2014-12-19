0f856be8184336bb846f7d947fb271ec4b56b7a0
molgin <molgin@users.noreply.github.com>
2014-12-18 22:41:25 -0500

    add another method

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -8,4 +8,10 @@ class Subcategory < ActiveRecord::Base
     :one_day_ago  => Time.now - 1.day)
   end
 
+  def changes_in_last_n_days(num)
+    # this should be in some kinda module
+    versions.where('created_at >= :n_days_ago',
+    :n_days_ago  => Time.now - num.days)
+  end
+
 end
